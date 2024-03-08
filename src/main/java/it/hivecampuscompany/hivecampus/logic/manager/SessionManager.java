ba5e15f7bc82893e0a8ba574e58a212baed36a5d
fabiobf96 <barchiesifabio96@gmail.com>
2024-03-08 20:37:01 +0100

    edit pattern singleton

--- src/main/java/it/hivecampuscompany/hivecampus/logic/manager/SessionManager.java
+++ src/main/java/it/hivecampuscompany/hivecampus/logic/manager/SessionManager.java
@@ -5,10 +5,22 @@ import it.hivecampuscompany.hivecampus.logic.model.Session;
 import it.hivecampuscompany.hivecampus.logic.model.User;
 
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SessionManager {
     private final HashMap<Integer, Session> sessionHashMap;
     private static SessionManager instance;
+    private static final Logger LOGGER = Logger.getLogger(SessionManager.class.getName());
+
+    static {
+        try {
+            instance = new SessionManager();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Exception while creating singleton instance", e);
+            System.exit(4);
+        }
+    }
 
     private SessionManager(){
         sessionHashMap = new HashMap<>();
