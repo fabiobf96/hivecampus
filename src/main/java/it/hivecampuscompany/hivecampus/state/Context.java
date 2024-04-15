b602535e8000a7781560ad07f8cf6c23c2b633cc
fabiobf96 <barchiesifabio96@gmail.com>
2024-04-15 14:33:52 +0200

    removed cli implementation state for create new branch

--- src/main/java/it/hivecampuscompany/hivecampus/state/Context.java
+++ src/main/java/it/hivecampuscompany/hivecampus/state/Context.java
@@ -2,9 +2,11 @@ package it.hivecampuscompany.hivecampus.state;
 
 import it.hivecampuscompany.hivecampus.bean.SessionBean;
 import it.hivecampuscompany.hivecampus.exception.InvalidSessionException;
+import javafx.stage.Stage;
 
 public class Context {
     private State state;
+    private Stage stage;
     private SessionBean sessionBean;
     public void setState(State state) {
         this.state = state;
@@ -16,6 +18,14 @@ public class Context {
 
     public void setSessionBean(SessionBean sessionBean) {
         this.sessionBean = sessionBean;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
 
     public void request() {
