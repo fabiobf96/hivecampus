c020f2df3be94b87069452298b09f72a7489b5d6
fabiobf96 <barchiesifabio96@gmail.com>
2024-04-26 12:30:59 +0200

    Update behavior method retrieveAdsByOwner in AdDAOCSV and update constructor in Ad

--- src/main/java/it/hivecampuscompany/hivecampus/model/Ad.java
+++ src/main/java/it/hivecampuscompany/hivecampus/model/Ad.java
@@ -11,28 +11,20 @@ public class Ad {
     private AdStart adStart;
     private final int price;
 
-    public Ad(int id, int adStatus, int price) {
-        this.id = id;
-        this.adStatus = AdStatus.fromInt(adStatus);
-        this.price = price;
-    }
-
     public Ad(int id, Home home, Room room, int price) {
         this.id = id;
         this.home = home;
         this.room = room;
         this.price = price;
     }
-
-    public AdBean toBasicBean() {
-        return new AdBean(id, home.toBasicBean(), room.toBasicBean(), price);
-    }
-
-    public Ad(int id, Account owner, Home home, Room room, int adStatus, int adStart, int price) {
+    public Ad(int id, Home home, Room room, int adStatus, int adStart, int price) {
         this(id, home, room, price);
-        this.owner = owner;
         this.adStatus = AdStatus.fromInt(adStatus);
         this.adStart = AdStart.fromInt(adStart);
+    }
+    public Ad(int id, Account owner, Home home, Room room, int adStatus, int adStart, int price) {
+        this(id, home, room, adStatus, adStart, price);
+        this.owner = owner;
     }
 
     public int getId() {
@@ -65,5 +57,8 @@ public class Ad {
 
     public int getPrice() {
         return price;
+    }
+    public AdBean toBasicBean() {
+        return new AdBean(id, home.toBasicBean(), room.toBasicBean(), price);
     }
 }
