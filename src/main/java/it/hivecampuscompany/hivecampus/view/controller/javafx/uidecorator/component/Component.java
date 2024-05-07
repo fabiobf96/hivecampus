7e44f06a3047ef09fcfe3655bc4c6572d87e47d0
fabiobf96 <barchiesifabio96@gmail.com>
2024-05-07 18:18:17 +0200

    Add missed classes after merge

---
+++ src/main/java/it/hivecampuscompany/hivecampus/view/controller/javafx/uidecorator/component/Component.java
@@ -0,0 +1,7 @@
+package it.hivecampuscompany.hivecampus.view.controller.javafx.uidecorator.component;
+
+import javafx.scene.Node;
+
+public abstract class Component {
+    public abstract Node setup();
+}
